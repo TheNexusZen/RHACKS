0e2d5963d9ad0b546a2261a2110e847815fa4aff
NexusZenLmao <nexzenomg@gmail.com>
2025-09-03 01:25:10 +0300

    Update sab.lua

--- sab.lua
+++ sab.lua
@@ -1,1 +1,13 @@
 local sab = 109983668079237
+local AkaliNotif = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/Dynissimo/main/Scripts/AkaliNotif.lua"))();
+local Notify = AkaliNotif.Notify;
+
+if game.PlaceId == sab then
+    Wow Code
+else
+    Notify({
+Description = "You Arent In Steal A Brainrot Or In The New Player Servers";
+Title = "Failed To Load | ErrorCode: 400";
+Duration = 5;
+});
+end
