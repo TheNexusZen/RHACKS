8892c239d71178818df8ca8be23346f85f3fdf08
NexusZenLmao <nexzenomg@gmail.com>
2025-09-19 03:36:10 +0300

    Update HUBWINDUI.lua

--- HUBWINDUI.lua
+++ HUBWINDUI.lua
@@ -2,6 +2,7 @@ local Version = "1.6.41"
 local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/download/" .. Version .. "/main.lua"))()
 local Players = game:GetService("Players")
 local plr = Players.LocalPlayer
+local UserInputService = game:GetService("UserInputService")
 local RunService = game:GetService("RunService")
 
 
@@ -126,52 +127,22 @@ RunService.RenderStepped:Connect(function()
 end
 
     end
---// Table to store original durations
-local DefaultDurations = {}
 
---// Single toggle button only
-local ProxToggle = Player:Toggle({
-    Title = "Instant Prox",
-    Desc = "Toggle Instant Proximity Prompts",
-    Icon = "tap",
-    Type = "Checkbox",
-    Default = false,
-    Callback = function(state) 
-        print("Instant Prox: " .. tostring(state))
-
-        -- loop existing prompts
-        for _, prompt in ipairs(workspace:GetDescendants()) do
-            if prompt:IsA("ProximityPrompt") then
-                if state then
-                    -- save once
-                    if not DefaultDurations[prompt] then
-                        DefaultDurations[prompt] = prompt.HoldDuration
-                    end
-                    prompt.HoldDuration = 0
-                else
-                    -- restore if saved
-                    if DefaultDurations[prompt] then
-                        prompt.HoldDuration = DefaultDurations[prompt]
-                    end
-                end
+local InfJump = Player:Button({
+    Title = "Infinite Jump",
+    Desc = "When Pressed You Can Jump Infinitly!",
+    Locked = false,
+    Callback = function()
+        UserInputService.JumpRequest:Connect(function()
+            local plr = game.Players.LocalPlayer
+            local hum = plr.Character and plr.Character:FindFirstChildOfClass("Humanoid")
+            if hum then
+                hum:ChangeState(Enum.HumanoidStateType.Jumping)
             end
-        end
+        end)
     end
 })
 
---// handle new prompts created later
-workspace.DescendantAdded:Connect(function(obj)
-    if obj:IsA("ProximityPrompt") then
-        if ProxToggle.Value then
-            if not DefaultDurations[obj] then
-                DefaultDurations[obj] = obj.HoldDuration
-            end
-            obj.HoldDuration = 0
-        end
-    end
-end)
-
-    
     
     
     
